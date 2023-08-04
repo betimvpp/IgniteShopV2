2e9d358f0a7401d025907de04cc00718a42be4b4
Roberto Junior <betimvpp@gmail.com>
2023-08-04 12:45:41 -0300

    second commit

--- src/pages/Products/[id].tsx
+++ src/pages/Products/[id].tsx
@@ -43,23 +43,23 @@ export default function Products  ({product}: ProductProps)  {
   return (
     <>
       <Head>
-        <title>{product.name} | Ignite Shop</title>
+        <title>{product?.name} | Ignite Shop</title>
       </Head>
 
       <div className="h-full min-h-656 grid grid-cols-2 mb-auto grid-rows-1 items-stretch gap-16 max-w-6xl w-full">   
           {/* Image Container */}
           <div className="max-w-xl w-full h-656 flex items-center overflow-hidden justify-center flex-col p-1 relative rounded-lg bg-gradient-to-b from-cyan-500 from-0% to-purple-500 to-100%">
-            <Image className="object-cover" src={product.imageUrl} alt={""}  width={520} height={480}/>
+            <Image className="object-cover" src={product?.imageUrl} alt={""}  width={520} height={480}/>
           </div>
           {/* Details Container */}
           <div className="p-4 pb-0 flex flex-col">
             <div>
-              <h1 className="font-bold text-3xl mb-4">{product.name}</h1>
-              <h2 className="text-green-500 mb-10 text-price ">{product.price}</h2>
+              <h1 className="font-bold text-3xl mb-4">{product?.name}</h1>
+              <h2 className="text-green-500 mb-10 text-price ">{product?.price}</h2>
             </div>
             
             <p className="text-lg text-gray-300 leading-7">
-            {product.description}
+            {product?.description}
             </p>
 
             <button onClick={handleCheckout} className="mt-auto w-full bg-green-500 border-none text-white rounded-lg p-5 cursor-pointer  font-bold text-lg enabled:hover:bg-green-600 enabled:duration-200  disabled:opacity-60 disabled:cursor-not-allowed" disabled={isCreatingCheckoutSession}>Comprar Agora</button>
